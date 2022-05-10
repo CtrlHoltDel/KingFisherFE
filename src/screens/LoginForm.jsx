99cfda3b6faa1918c354d95120765909d5045c66
CtrlHoltDel <CtrlHoltDel@gmail.com>
2022-05-10 20:57:35 +0100

    Updated login, added post req for register

--- src/screens/LoginForm.jsx
+++ src/screens/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { register } from "../api/actions";
 import logo from "../assets/images/logo.png";
 import "../style/loginform/loginform.css";
 
@@ -7,21 +8,30 @@ import "../style/loginform/loginform.css";
 const LoginForm = ({ setUser }) => {
 
   const [loginRegisterType, setLoginRegisterType] = useState("login")
-  const [credentials, setCredentials] = useState({ username: null, password: null, repeatedPassword: null})
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [validatePassword, setValidatePassword] = useState("");
+
 
   const toggleType = () => setLoginRegisterType(loginRegisterType === "login" ? "register" : "login")
 
-  const handleSubmitForm = (e) => {
+  const handleSubmitForm = async (e) => {
     e.preventDefault();
 
-    console.log(credentials);
+
     if(loginRegisterType === 'login'){
+      // setUser({username: "test", password: "test"})
       //handle login
     } else {
-      //hanlde register
-    }
+      // setLoginRegisterType("login")
+      //handle register
+      const response = await register(username, password)
 
+      console.log(response)
+    }
   }
+
+  
 
   return (
     <div className="login-container">
@@ -30,15 +40,21 @@ const LoginForm = ({ setUser }) => {
       </div>
       <form onSubmit={handleSubmitForm} className="login-container__right sub-container">
         <label htmlFor="">Username</label>
-        <input type="text" />
+        <input type="text" value={username} onChange={(e) => {
+          setUsername(e.target.value)
+        }}/>
         <label htmlFor="">Password</label>
-        <input type="text" />
+        <input type="text" value={password} onChange={(e) => {
+          setPassword(e.target.value)
+        }}/>
         {loginRegisterType === "register" &&  <>
         <label htmlFor="">Repeat password</label>
-        <input type="text" />
+        <input type="text" value={validatePassword} onChange={(e) => {
+          setValidatePassword(e.target.value)
+        }}/>
         </>}
         <button>{loginRegisterType === "login" ? "Log in" : "Register"}</button>
-        <p onClick={toggleType} class="register-login-toggle">{loginRegisterType === "login" ? "Don't have an account? Register" : "Already have an account? Log in."}</p>
+        <p onClick={toggleType} className="register-login-toggle">{loginRegisterType === "login" ? "Don't have an account? Register" : "Already have an account? Log in."}</p>
       </form>
     </div>
   );
